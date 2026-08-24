1364eedbe7ddbb316ad3676460e0ff2dcb9ac8ec
m-uvex <musamurad@proton.me>
2026-08-24 15:45:54 +0300

    feat: Added illogical impulse & end4-pC remove: Deleted stray pieces of the broken removed multi dots system

--- flake.nix
+++ flake.nix
@@ -8,6 +8,16 @@
     zen-browser.url = "github:youwen5/zen-browser-flake";
     nix-software-center.url = "github:snowfallorg/nix-software-center";
     driftwm.url = "github:malbiruk/driftwm";
+
+    # Illogical Impulse & end4-pC
+    illogical-flake = {
+      url = "github:soymou/illogical-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    end4-pC = {
+      url = "github:pctrade/end4-pC";
+      flake = false;
+    };
   };
 
   outputs = { self, nixpkgs, driftwm, ... }@inputs:
@@ -17,11 +27,10 @@
     specialArgs = { inherit inputs; };
   in {
 
-
     nixosConfigurations = {
       #=========================================#
-      #                  Orion                  #
-      #          Laptop: HP 15, NixOS           #
+      #                 Orion                   #
+      #         Laptop: HP 15, NixOS            #
       #=========================================#
       lt-hp15-nix = nixpkgs.lib.nixosSystem {
         inherit system specialArgs;
@@ -32,7 +41,7 @@
       };
 
       #=========================================#
-      #                Andromeda                #
+      #               Andromeda                 #
       #   PC: Main (RTX5050, R5 5600), NixOS    #
       #=========================================#
       pc-main-nix = nixpkgs.lib.nixosSystem {
@@ -44,8 +53,8 @@
       };
 
       #=========================================#
-      #                  Lunar                  #
-      #    Server: Lenovo AIO C40-30, NixOS     #
+      #                 Lunar                   #
+      #   Server: Lenovo AIO C40-30, NixOS      #
       #=========================================#
       srv-c4030-nix = nixpkgs.lib.nixosSystem {
         inherit system specialArgs;
@@ -55,40 +64,32 @@
       };
     };
 
-    homeConfigurations = {
       #=========================================#
-      #             Caelestia Shell             #
+      #              end4-pC Shell              #
       #=========================================#
-      caelestia = mkRice {
+      end4-pc = mkRice {
         extraModules = [
-          inputs.caelestia-shell.homeManagerModules.default
+          inputs.illogical-flake.homeManagerModules.default
+          {
+            # Enable the base illogical-impulse framework
+            programs.illogical-impulse = {
+              enable = true;
+              dotfiles = {
+                fish.enable = true;
+                kitty.enable = true;
+                starship.enable = true;
+              };
+            };
+            xdg.configFile."quickshell/end4-pC".source = inputs.end4-pC;
+            home.sessionVariables = {
+              qsConfig = "end4-pC";
+            };
+          }
         ];
         extraHyprlandConfig = ''
-          exec-once = caelestia
-        '';
-      };
-
-      #=========================================#
-      #             Midnight Shell              #
-      #=========================================#
-      midnight = mkRice {
-        extraModules = [
-          inputs.midnight-shell.homeManagerModules.default
-        ];
-        extraHyprlandConfig = ''
-          exec-once = midnight
-        '';
-      };
-
-      #=========================================#
-      #           DankMaterialShell             #
-      #=========================================#
-      dms = mkRice {
-        extraModules = [
-          inputs.dms.homeManagerModules.default
-        ];
-        extraHyprlandConfig = ''
-          exec-once = dms
+          # Ensure Hyprland injects the profile variable and binds the settings menu
+          env = qsConfig,end4-pC
+          bind = SUPER, ESCAPE, global, quickshell:settingsToggle
         '';
       };
     };
