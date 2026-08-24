ccfbbb85d452a4d5673b05d62c188a8b1a946dac
m-uvex <musamurad@proton.me>
2026-08-24 15:34:09 +0300

    remove: Got rid of the multi-dotfiles fuckery, ts js aint working vro

--- flake.nix
+++ flake.nix
@@ -4,63 +4,20 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     # Custom Software
     zen-browser.url = "github:youwen5/zen-browser-flake";
     nix-software-center.url = "github:snowfallorg/nix-software-center";
-
-    # DEs / WMs
     driftwm.url = "github:malbiruk/driftwm";
-
-    # Rices
-    midnight-shell.url = "github:dim-ghub/midnight-shell";
-    caelestia-shell.url = "github:caelestia-dots/shell";
-    dms.url = "github:AvengeMedia/DankMaterialShell";
   };
 
-  outputs = { self, nixpkgs, home-manager, driftwm, ... }@inputs:
+  outputs = { self, nixpkgs, driftwm, ... }@inputs:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     specialArgs = { inherit inputs; };
-
-    # Standalone Home Manager helper
-    mkRice = { extraModules ? [], extraHyprlandConfig ? "" }: home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      extraSpecialArgs = { inherit inputs; };
-      modules = [
-        {
-          home.username = "m_uvex";
-          home.homeDirectory = "/home/m_uvex";
-          home.stateVersion = "24.05";
-
-          # Declaratively manage Hyprland dotfiles
-          wayland.windowManager.hyprland = {
-            enable = true;
-            settings = {
-              "$mod" = "SUPER";
-              bind = [
-                "$mod, Q, exec, kitty"
-                "$mod, C, killactive,"
-                "$mod, M, exit,"
-                "$mod, E, exec, nautilus"
-                "$mod, V, togglefloating,"
-                "$mod, R, exec, rofi -show drun"
-              ];
-            };
-            extraConfig = ''
-              monitor=,preferred,auto,auto
-              ${extraHyprlandConfig}
-            '';
-          };
-        }
-      ] ++ extraModules;
-    };
   in {
+
+
     nixosConfigurations = {
       #=========================================#
       #                  Orion                  #
