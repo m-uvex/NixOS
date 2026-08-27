330e912f1d5c03ad71fdd38d9c4d7bf5ee42fd0c
m_uvex <musamurad@proton.me>
2026-08-27 17:59:51 +0300

    fix: Fixed end4-pC, now it loads on top of ii!!!!

--- modules/home.nix
+++ modules/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, inputs, lib, ... }:
 
 {
   # --- WALLPAPERS ---
@@ -23,6 +23,22 @@
   home.sessionVariables = {
     qsConfig = "end4-pC";
   };
+
+  home.activation.configureEnd4pC = lib.hm.dag.entryAfter [ "copyIllogicalImpulseConfigs" ] ''
+    mkdir -p "$HOME/.config/hypr/custom"
+
+    cat > "$HOME/.config/hypr/custom/variables.lua" << 'EOF'
+-- Set active quickshell configuration to end4-pC
+hl.env("qsConfig", "end4-pC")
+EOF
+
+    cat > "$HOME/.config/hypr/custom/keybinds.lua" << 'EOF'
+hl.bind("CTRL+SUPER+ALT+Slash", hl.dsp.exec_cmd("xdg-open ~/.config/hypr/custom/keybinds.lua"), {description = "Edit user keybinds"} )
+hl.bind("SUPER + escape", hl.dsp.global("quickshell:settingsToggle"), {description = "Toggle settings"})
+EOF
+
+    chmod -R u+w "$HOME/.config/hypr/custom"
+  '';
 
   home.stateVersion = "24.05";
 }
